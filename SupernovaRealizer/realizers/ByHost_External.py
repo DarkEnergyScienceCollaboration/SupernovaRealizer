a229c4d0f172ae997fda5b8878d20bdb10926570
Alex Kim <agkim@lbl.gov>
2013-01-25 14:51:08 -0800

    Copy ByHost to ByHost_External

--- SupernovaRealizer/realizers/ByHost_External.py
+++ SupernovaRealizer/realizers/ByHost_External.py
@@ -8,10 +8,45 @@ import Sources
 import TabularSED
 import numpy
 
-name='ByHost_External'
+name = 'ByHost_External'
+
 
 class RealizerContainer(object):
+    def __init__(self, realizer, logical):
+        self.realizer = realizer
+        self.logical = logical
+        self.galaxyiterator = iter(self.realizer.galaxies)
+        self.sniterator = iter(RealizerContainerByGalaxy(realizer, self.galaxyiterator.next()))
+   #     print "Init ",self.sniterator.galaxy.id
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        done = False
+        while not done:
+            try:
+                thenext = self.sniterator.next()
+                if self.logical is None:
+                    done = True
+                    return thenext
+
+  #              print "next ",self.sniterator.galaxy.id
+                newlogical = 'thenext.' + self.logical
+                if eval(newlogical):
+                    done = True
+                    return thenext
+
+            except StopIteration as e1:
+                try:
+                    nextgal = self.galaxyiterator.next()
+                except StopIteration as e2:
+                    raise StopIteration
+                self.sniterator = iter(RealizerContainerByGalaxy(self.realizer, nextgal))
+
+class RealizerContainerByGalaxy(object):
     def __init__(self, realizer, galaxy):
+#        print realizer.seed, galaxy.id
         self.random_state = numpy.random.RandomState([realizer.seed, galaxy.id])
 
         self.realizer = realizer
@@ -27,13 +62,13 @@ class RealizerContainer(object):
         if self.index >= self.nsn:
             raise StopIteration
         else:
-            (ra, dec) = self.realizer.where.realize(self.random_state,self.galaxy)
+            (ra, dec) = self.realizer.where.realize(self.random_state, self.galaxy)
             redshift = self.galaxy.redshift
             doe = self.realizer.when.realize(self.random_state, self.galaxy)
             model = self.realizer.what
             nm = (self.galaxy.id, self.index)
             self.index += 1
-            return Sources.Supernova(ra, dec, redshift, doe, model,self.galaxy, nm)
+            return Sources.Supernova(ra, dec, redshift, doe, model, self.galaxy, nm)
 
 class When(object):
     '''
@@ -51,7 +86,7 @@ class When(object):
 
     def number(self, random_state, galaxy):
 
-        expected = When.rate * (self.sim_end - self.sim_start)/(1 + galaxy.redshift)
+        expected = When.rate * (self.sim_end - self.sim_start) / (1 + galaxy.redshift)
 
         # calculate the number of explosions in the time period
         return random_state.poisson(expected)
@@ -77,32 +112,50 @@ class Where(object):
         self.radius = radius
 
     def realize(self, random_state, galaxy):
-        return (galaxy.ra, galaxy.dec) + random_state.normal(scale=self.radius,size=2)
+        return (galaxy.ra, galaxy.dec) + random_state.normal(scale=self.radius, size=2)
 
 class Realizer(object):
     '''
-    classdocs
+    Supernova realizer that generates objects given an input galaxy.  Supernovae are discovered
+    with uniform probability over a time window given a constant local rate.  Supernovae
+    are placed within a Gaussian distribution around the input gaalxy.  The supernova model
+    is based on the interpolation of tabular (phase,wavelength,flux) data.
+    
+    The realizer instantiation is specified by a set of parameters
+    
+     seed                        seed for random number generator
+     when.sim_start                start of time window in which SNe are generated
+     when.sim_end                    end of time window in which SNe are generated
+     where.radius                supernovae are randomly distributed in a Gaussian
+                                 distribution around the galaxy with this sigma
+     what.name                    name+'.pkl' is the file that contains (phase,wavelength,flux) information
+     what.normalization        normalization applied to the flux in the file to give luminosity
     '''
     parameter_names = ['seed']
-    parameter_names+=(When.parameter_names+Where.parameter_names+TabularSED.TabularSED.parameter_names)
-    
-    def __init__(self,pars):
+    parameter_names += (When.parameter_names + Where.parameter_names + TabularSED.TabularSED.parameter_names)
+
+    def __init__(self, galaxies, pars):
 
         '''
         Constructor
         '''
+        self.galaxies = galaxies
+
         self.seed = pars[Realizer.parameter_names[0]]
         self.sim_start = pars[Realizer.parameter_names[1]]
         self.sim_end = pars[Realizer.parameter_names[2]]
         self.radius = pars[Realizer.parameter_names[3]]
         self.sedname = pars[Realizer.parameter_names[4]]
-        self.normalization=pars[Realizer.parameter_names[5]]
+        self.normalization = pars[Realizer.parameter_names[5]]
 
         self.when = When(self.sim_start, self.sim_end)
         self.where = Where(self.radius)
         self.what = TabularSED.TabularSED(self.sedname, self.normalization)
-        
 
-    def realize(self, galaxy):
+    def realize(self, logical):
 
-        return RealizerContainer(self, galaxy)
+        return RealizerContainer(self, logical)
+
+    def realizeByGalaxy(self, galaxy):
+
+        return RealizerContainerByGalaxy(self, galaxy)
